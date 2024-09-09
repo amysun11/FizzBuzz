b94984c1ab50432dbe0009b36c6ac86e8d494565
amysun1 <manrong.sun@mail.utoronto.ca>
2024-09-09 01:20:47 -0400

    added main method

---
+++ src/Multiples.java
@@ -0,0 +1,31 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        int count = 0;
+        while (i < 1000){
+
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                count += 1;
+
+            } else if (divisibleBy3) {
+
+                count += 1;
+
+            } else if (divisibleBy5) {
+
+                count += 1;
+
+            }
+            i = i + 1;
+
+        }
+        System.out.println(count);
+    }
+}
